aed4f726e3290531118a1ed6846190bab7484091
Jeremiasz <jeremiasz.zrolka@gmail.com>
2021-01-31 16:03:24 +0100

    Added chart for daily accesses and improved up-time chart

--- Website/script/showdata.js
+++ Website/script/showdata.js
@@ -1,101 +1,147 @@
 'use strict';
 
-let mylabels = [];
-let myhours = [];
-
-getUpHours()
-
-function getUpHours() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let data = JSON.parse(this.responseText);
-            data.data.forEach((element) => {
-                mylabels.push(element.date);
-                myhours.push(element.hours);
-            });
-            myChart.update();
-        }
-    }
-    xhttp.open("POST", "getUpHours.php", true)
-    xhttp.send()
-}
-
-
-let mycolors = ['#33E0E0', '#FFBD26', '#E04B3F'];
-let ctx = document.getElementById('upHoursChart').getContext('2d');
-let myChart = new Chart(ctx, {
+let mycolors = ['#FFBD26', '#E04B3F', '#bcd4ff', '#5393ff'];
+let ctxUptimeChart = document.getElementById('upTimeChart').getContext('2d');
+let uptimeChart = new Chart(ctxUptimeChart, {
     type: 'bar',
     data: {
-        labels: mylabels,
+        labels: [],
         datasets: [
             {
-                label: 'hours online',
-                data: myhours,
-                backgroundColor: function (context) {
-                    let value = context.dataset.data[context.dataIndex];
-                    return mycolors[Math.ceil(value / 8) - 1];
-                },
+                label: 'Raspberry',
+                data: [],
+                backgroundColor: mycolors[0],
                 borderWidth: 0,
-                maxBarThickness: 20,
+                maxBarThickness: 20
             },
+            {
+                label: 'Accesspoint',
+                data: [],
+                backgroundColor: mycolors[1],
+                borderWidth: 0,
+                maxBarThickness: 20
+            }
         ]
     },
     options: {
+        defaultFontFamily: Chart.defaults.global.defaultFontFamily = "'Poppins'",
+        maintainAspectRatio: false,
         title: {
             display: true,
-            text: 'UP-Time of Raspberry',
+            text: 'UP-Time',
             fontSize: 16,
-            fontColor: '#242424',
-            fontFamily: 'Poppins'
-        },
-        legend: {
-            position: 'right',
-            labels: {
-                fontFamily: 'Poppins',
-                generateLabels: function (context) {
-                    let hidden = context.data.datasets[0]._meta[0].hidden;
-                    return [
-                        {
-                            text: '<8 hours',
-                            datasetIndex: 0,
-                            fillStyle: mycolors[0],
-                            hidden: hidden,
-                            strokeStyle: 'rgba(0,0,0,0)',
-                        },
-                        {
-                            text: ' 8-16 hours',
-                            datasetIndex: 0,
-                            fillStyle: mycolors[1],
-                            hidden: hidden,
-                            strokeStyle: 'rgba(0,0,0,0)',
-                        },
-                        {
-                            text: '16-24 hours',
-                            datasetIndex: 0,
-                            fillStyle: mycolors[2],
-                            hidden: hidden,
-                            strokeStyle: 'rgba(0,0,0,0)',
-                        }];
-                }
-            }
+            fontColor: '#242424'
         },
         scales: {
-            xAxes: [{
-                ticks: {
-                    fontFamily: 'Poppins',
-                },
-            }],
             yAxes: [{
                 ticks: {
                     beginAtZero: true,
                     max: 24,
                     callback: function (value, index, values) {
                         return value + "h";
-                    },
-                    fontFamily: 'Poppins',
-                },
-            }],
+                    }
+                }
+            }]
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    if (tooltipItem.datasetIndex === 0) {
+                        return `Raspberry was ${tooltipItem.value} hours online`;
+                    } else if (tooltipItem.datasetIndex === 1) {
+                        return `AP was ${tooltipItem.value} hours online`;
+                    }
+                }
+            }
         }
-    },
+    }
 });
+
+
+let ctxAccessCounterChart = document.getElementById('accessCounterChart').getContext('2d');
+let accessCounterChart = new Chart(ctxAccessCounterChart, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [
+            {
+                borderColor: mycolors[2],
+                data: [],
+                fill: false,
+                label: 'Accesses',
+                pointBackgroundColor: mycolors[3],
+                pointBorderColor: mycolors[3],
+                pointRadius: 3.5
+            }
+        ]
+    },
+    options: {
+        defaultFontFamily: Chart.defaults.global.defaultFontFamily = "'Poppins'",
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Accesses',
+            fontSize: 16,
+            fontColor: '#242424'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    return `The site was accessed ${tooltipItem.value} times`;
+                }
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    callback: function (value, index, values) {
+                        console.log(index)
+                        if (index % 2 === 0) {
+                            return value;
+                        } else {
+                            return null;
+                        }
+                    }
+                }
+            }]
+        },
+    }
+});
+
+
+function getUpHours() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText);
+            uptimeChart.data.labels = data.data.map(x => x.date);
+            uptimeChart.data.datasets[0].data = data.data.map(x => x.raspberryUptime);
+            uptimeChart.data.datasets[1].data = data.data.map(x => x.apUptime);
+            uptimeChart.update();
+
+            let len = accessCounterChart.data.labels.length
+            accessCounterChart.data.labels = data.data.slice(0, len).map(x => x.date);
+            accessCounterChart.data.datasets[0].data = data.data.slice(0, len).map(x => Math.random() * 50);
+            accessCounterChart.update();
+
+            for (let i = len; i < data.data.length; i++) {
+                accessCounterChart.data.labels.push(data.data[i].date);
+                accessCounterChart.data.datasets[0].data.push(0);
+                accessCounterChart.update();
+            }
+
+
+            for (let i = len; i < data.data.length; i++) {
+                accessCounterChart.data.datasets[0].data[i] = data.data[i].accesses;
+                accessCounterChart.update();
+            }
+        }
+    }
+
+    xhttp.open("POST", "getUpHours.php", true);
+    xhttp.send();
+}
