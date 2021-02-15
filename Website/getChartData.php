8ab0e6b4131ebeb8cfab2dd032c6d5fe9825fda5
Jeremiasz <jeremiasz.zrolka@gmail.com>
2021-02-15 12:47:34 +0100

    Updated data generation (data is more realistic)

--- Website/getChartData.php
+++ Website/getChartData.php
@@ -4,21 +4,34 @@
 //$output = json_encode($output);
 $output = '{"data":[';
 
+$counter = 2;
+
 for ($i = 0; $i < 100; $i++) {
     $output .= '{';
     $output .= '"utcDate": ' . (time() + 86000 * $i) * 1000 . ',';
-    $output .= '"raspberryUptime": ' . rand(0, 24) . ',';
-    $output .= '"apUptime": ' . rand(0, 24) . ',';
-    $output .= '"accesses": ' . random_int(10, 100);
+
+    $uptime = rand(5, 24);
+    $output .= '"raspberryUptime": ' . $uptime . ',';
+    $output .= '"apUptime": ' . rand(0, $uptime) . ',';
+
+    $output .= '"accesses": ' . random_int($counter, $counter + 40);
+    $counter += 10;
+
     $output .= '},';
 }
 
 $output .= '{';
 $output .= '"utcDate": ' . (time() + 86000 * $i) * 1000 . ',';
-$output .= '"raspberryUptime": ' . rand(0, 24) . ',';
-$output .= '"apUptime": ' . rand(0, 24) . ',';
-$output .= '"accesses": ' . random_int(10, 100);
+
+$uptime = rand(5, 24);
+$output .= '"raspberryUptime": ' . $uptime . ',';
+$output .= '"apUptime": ' . rand(0, $uptime) . ',';
+
+$output .= '"accesses": ' . random_int($counter, $counter + 40);
+$counter += 10;
+
 $output .= '}]}';
+
 
 header('Content-Type: application/json');
 
