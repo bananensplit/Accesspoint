19061d178f67eb594925cfefb1ccfa35813ab585
Jeremiasz <jeremiasz.zrolka@gmail.com>
2021-03-20 23:59:22 +0100

    added 'loading animation' to charts

--- web/script/showdata.js
+++ web/script/showdata.js
@@ -111,6 +111,11 @@ let accessCounterChart = new Chart(ctxAccessCounterChart, {
     }
 });
 
+animateData()
+let animateDiagrams = setInterval(animateData, 750);
+
+
+// TODO: make dataAmount work
 function updateCharts(dataAmount = 100) {
     if (dataAmount < 14) dataAmount = 14;
 
@@ -118,6 +123,7 @@ function updateCharts(dataAmount = 100) {
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             let data = JSON.parse(this.responseText);
+            clearInterval(animateDiagrams)
 
             if (uptimeChart.data.datasets.length <= 0) {
                 let raspberryUptime = {
@@ -180,5 +186,57 @@ function secToHourAndRound(seconds, decimals = 2) {
     let hours = seconds / 60 / 60;
     return Math.round(hours * decimals) / decimals;
 }
+
+
+function animateData(daysBack = 14) {
+    let data = []
+    for (let i = daysBack - 1; i >= 0; i--) {
+        data.push({
+            "date": moment().subtract(i, 'days').format("YYYY-MM-DD"),
+            "raspberryUptime": Math.random() * 86400,
+            "apUptime": Math.random() * 86400,
+            "accesses": Math.floor(Math.random() * 40)
+        })
+    }
+
+    if (uptimeChart.data.datasets.length <= 0) {
+        let raspberryUptime = {
+            label: 'Raspberry',
+            data: data.map(x => ({x: x.date, y: secToHourAndRound(x.raspberryUptime)})),
+            backgroundColor: mycolors[0],
+        };
+        let apUptime = {
+            label: 'Accesspoint',
+            data: data.map(x => ({x: x.date, y: secToHourAndRound(x.apUptime)})),
+            backgroundColor: mycolors[1],
+        };
+        uptimeChart.data.datasets.push(raspberryUptime, apUptime);
+        uptimeChart.update();
+    } else {
+        uptimeChart.data.datasets[0].data = data.map(x => ({x: x.date, y: secToHourAndRound(x.raspberryUptime)}));
+        uptimeChart.data.datasets[1].data = data.map(x => ({x: x.date, y: secToHourAndRound(x.apUptime)}));
+        uptimeChart.update();
+    }
+
+    if (accessCounterChart.data.datasets.length <= 0) {
+        let dailyAccesses = {
+            label: 'Accesses',
+            data: data.map(x => ({x: x.date, y: x.accesses})),
+            borderColor: mycolors[3],
+            borderWidth: 2,
+            lineTension: 0,
+            backgroundColor: mycolors[2],
+            pointBackgroundColor: mycolors[3],
+            pointBorderColor: mycolors[3],
+            pointRadius: 0
+        };
+        accessCounterChart.data.datasets.push(dailyAccesses);
+        accessCounterChart.update();
+    } else {
+        accessCounterChart.data.datasets[0].data = data.map(x => ({x: x.date, y: x.accesses}));
+        accessCounterChart.update();
+    }
+}
+
 
 updateCharts();
