eccaeed09b44846a9a94bd4ac3d4404797a55753
Jeremiasz <jeremiasz.zrolka@gmail.com>
2022-01-28 15:02:08 +0100

    refactored

--- backend/ap-api.py
+++ backend/ap-api.py
@@ -3,15 +3,15 @@
 import asyncio
 import json
 import logging
+import re
 import subprocess as sp
 import time
 from datetime import datetime
+from functools import cache
 from logging import handlers
 
 import pandas as pd
 import websockets
-
-from iw_parser import parse_station_dump
 
 
 async def handler(websocket):
@@ -123,6 +123,41 @@ def turn_off():
             'success': process.returncode == 0
         }
     })
+
+
+def parse_station_dump(output):
+    lines = output.strip("\n").split("\n")
+    mac_address = re.search(r'([0-9a-fA-F]:?){12}', lines[0]).group()
+
+    lines = [re.sub(r"\s", "", line) for line in lines[1:]]
+    lines = [line.split(":") for line in lines]
+    lines = {line[0]: line[1] for line in lines}
+
+    return {
+        'mac': mac_address,
+        'vendor': get_vendor_name(mac_address),
+        'rx-bitrate': lines["rxbitrate"],
+        'tx-bitrate': lines["txbitrate"],
+        'connected-time': lines["connectedtime"][:-7],
+    }
+
+
+@cache
+def get_mac_addresses(file="resources/mac_addresses_28-01-2022.csv"):
+    return pd.read_csv(file, sep=";", encoding_errors='ignore')
+
+
+@cache
+def get_vendor_name(mac_address):
+    """
+        mac_address format: 
+            AA-BB-CC-DD-EE-FF
+            AA:BB:CC:DD:EE:FF
+    """
+    macs = get_mac_addresses()
+    mac = re.sub(r"[-:]", "", mac_address)[0:6].upper()
+    erg = macs.loc[macs["Assignment"] == mac, "Organization Name"].values
+    return None if len(erg) >= 0 else erg[0]
 
 
 async def main():
