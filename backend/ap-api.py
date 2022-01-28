eabf8da25baff131caa8857c578bcd7ba14bf4ac
Jeremiasz <jeremiasz.zrolka@gmail.com>
2022-01-28 14:06:44 +0100

    added clients infos to api

--- backend/ap-api.py
+++ backend/ap-api.py
@@ -14,6 +14,8 @@ from re import split
 import pandas as pd
 import websockets
 
+from iw_parser import parse_station_dump
+
 
 async def handler(websocket):
     while True:
@@ -30,6 +32,8 @@ async def handler(websocket):
             await websocket.send(turn_on())
         elif data['type'] == "off":
             await websocket.send(turn_off())
+        elif data['type'] == "clients_info":
+            await websocket.send(clients_info())
 
 
 def hello_world():
@@ -45,26 +49,32 @@ def hello_world():
 def info():
     def ap_state():
         command = 'systemctl is-active hostapd'
-        process = sp.Popen(command.split(' '), stdout=sp.PIPE, stderr=sp.STDOUT)
+        process = sp.Popen(command.split(
+            ' '), stdout=sp.PIPE, stderr=sp.STDOUT)
         return process.stdout.read().decode('ascii').replace('\n', '')
 
     def pi_uptime():
         command = 'uptime -s'
-        process = sp.Popen(command.split(' '), stdout=sp.PIPE, stderr=sp.STDOUT)
+        process = sp.Popen(command.split(
+            ' '), stdout=sp.PIPE, stderr=sp.STDOUT)
         data = process.stdout.read().decode('ascii').replace('\n', '')
         return datetime.timestamp(datetime.strptime(data, '%Y-%m-%d %H:%M:%S'))
 
     def ap_uptime():
         command = 'systemctl show hostapd --property=ActiveEnterTimestamp --value'
-        process = sp.Popen(command.split(' '), stdout=sp.PIPE, stderr=sp.STDOUT)
+        process = sp.Popen(command.split(
+            ' '), stdout=sp.PIPE, stderr=sp.STDOUT)
         data = process.stdout.read().decode('ascii').replace('\n', '')
         return datetime.timestamp(datetime.strptime(data, '%a %Y-%m-%d %H:%M:%S %Z'))
 
     def clients():
-        command = 'iw dev wlan0 station dump | grep Station | wc -l'.split(' | ')
+        command = 'iw dev wlan0 station dump | grep Station | wc -l'.split(
+            ' | ')
         iw = sp.Popen(command[0].split(' '), stdout=sp.PIPE, stderr=sp.STDOUT)
-        grep = sp.Popen(command[1].split(' '), stdin=iw.stdout, stdout=sp.PIPE, stderr=sp.STDOUT)
-        wc = sp.Popen(command[2].split(' '), stdin=grep.stdout, stdout=sp.PIPE, stderr=sp.STDOUT)
+        grep = sp.Popen(command[1].split(
+            ' '), stdin=iw.stdout, stdout=sp.PIPE, stderr=sp.STDOUT)
+        wc = sp.Popen(command[2].split(
+            ' '), stdin=grep.stdout, stdout=sp.PIPE, stderr=sp.STDOUT)
         return int(wc.stdout.read().decode('ascii').replace('\n', ''))
 
     return json.dumps({
@@ -76,6 +86,19 @@ def info():
             'ap-uptime': ap_uptime(),
             'clients': clients(),
         }
+    })
+
+
+def clients_info():
+    command = 'iw dev wlan0 station dump'
+    iw = sp.Popen(command.split(' '), stdout=sp.PIPE, stderr=sp.STDOUT)
+    data = iw.stdout.read().decode('ascii').strip("Station").split("Station")
+    data = [parse_station_dump(device) for device in data]
+
+    return json.dumps({
+        'timestamp': time.time() * 1000,
+        'type': 'info',
+        'data': data
     })
 
 
@@ -113,5 +136,6 @@ async def main():
 if __name__ == "__main__":
     logger = logging.getLogger('websockets')
     logger.setLevel(logging.WARN)
-    logger.addHandler(logging.handlers.RotatingFileHandler, "ap-websocket.log", maxBytes=10_000_000, backupCount=2, encoding='UTF-8')
+    logger.addHandler(logging.handlers.RotatingFileHandler(
+        "ap-websocket.log", maxBytes=10_000_000, backupCount=2, encoding='UTF-8'))
     asyncio.run(main())
